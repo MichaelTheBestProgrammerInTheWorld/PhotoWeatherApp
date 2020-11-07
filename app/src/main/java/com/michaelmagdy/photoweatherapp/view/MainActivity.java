abef52f2e2f525ee4a59e2cf177d7c6df5fe613b
Michael <michaelmagdy_93@yahoo.com>
2020-11-07 16:02:18 +0200

    adding expandable fab

--- app/src/main/java/com/michaelmagdy/photoweatherapp/view/MainActivity.java
+++ app/src/main/java/com/michaelmagdy/photoweatherapp/view/MainActivity.java
@@ -7,8 +7,11 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.michaelmagdy.photoweatherapp.R;
 import com.michaelmagdy.photoweatherapp.model.webservice.WeatherPojo;
 import com.michaelmagdy.photoweatherapp.viewmodel.MainActivityViewModel;
@@ -16,6 +19,10 @@ import com.michaelmagdy.photoweatherapp.viewmodel.MainActivityViewModel;
 public class MainActivity extends AppCompatActivity {
 
     private MainActivityViewModel mainActivityViewModel;
+
+    FloatingActionButton mAddFab, mCameraFab, mGalleryFab;
+    TextView cameraText, galleryText;
+    Boolean isAllFabsVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +47,62 @@ public class MainActivity extends AppCompatActivity {
                 + weatherPojo.getTemp() + "  " + weatherPojo.getDescription());
             }
         } );
+
+
+        mAddFab = findViewById(R.id.add_fab);
+        mCameraFab = findViewById(R.id.camera_fab);
+        mGalleryFab = findViewById(R.id.gallery_fab);
+        cameraText = findViewById(R.id.camera_text);
+        galleryText = findViewById(R.id.gallery_text);
+
+        mCameraFab.setVisibility(View.GONE);
+        mGalleryFab.setVisibility(View.GONE);
+        cameraText.setVisibility(View.GONE);
+        galleryText.setVisibility(View.GONE);
+
+        isAllFabsVisible = false;
+
+        mAddFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!isAllFabsVisible) {
+
+                            mCameraFab.show();
+                            mGalleryFab.show();
+                            cameraText.setVisibility(View.VISIBLE);
+                            galleryText.setVisibility(View.VISIBLE);
+
+                            isAllFabsVisible = true;
+                        } else {
+
+                            mCameraFab.hide();
+                            mGalleryFab.hide();
+                            cameraText.setVisibility(View.GONE);
+                            galleryText.setVisibility(View.GONE);
+
+                            isAllFabsVisible = false;
+                        }
+                    }
+                });
+
+
+        mGalleryFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Gallery Added", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        mCameraFab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Camera Added", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
+
+
 }
